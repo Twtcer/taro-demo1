4b7a15e88803122782cdb895b85fdafe9204fd6d
奉伟 <landwind1180@gmail.com>
2019-09-11 18:04:12 +0800

    add cart

---
+++ src/components/bottom/bottom.tsx
@@ -0,0 +1,53 @@
+import Taro, { Component } from '@tarojs/taro';
+import { View, Text, Button, Image } from '@tarojs/components';
+import './bottom.scss';
+
+import  {getAllFoodInfo,myEvent, getEvent} from '../../utils/utils';
+let events = getEvent;
+
+export default class Bottom extends Component {
+
+  config = {
+    navigationBarTitleText: ''
+  }
+  state = {
+    num: 0,
+    minPrice: 20,
+    sendPrice: 4,
+    bySelf: true,
+    allPrice:0, 
+  }
+
+  componentWillMount() { }
+  componentDidMount() {
+    //获取整体的存储的菜品数据进行计算
+    //获取计算好的值设置给state 
+    let { allPrice, allNum } = getAllFoodInfo();
+    this.setState({allPrice:allPrice,num:allNum});
+   }
+  componentWillReceiveProps(nextProps, nextContext) { }
+  componentWillUnmount() { }
+  componentDidShow() { }
+  componentDidHide() { }
+  componentDidCatchError() { }
+  componentDidNotFound() { }
+  render() {
+    let { num, sendPrice, bySelf, minPrice } = this.state;
+    return (
+      <View className='bottom'>
+        <View className='bottom_body'>
+          {num>0 ? <Text className='num'>{num}</Text> : null}
+          <Image className='cart' src={require('../../assets/image/bottom/cart_empty.png')}></Image>
+          <View className='info'>
+            {sendPrice ? <Text >另需配送费￥{sendPrice}</Text> : null}
+            |
+          {bySelf ? <Text > 支持自取</Text> : null}
+          </View>
+          <View className='submit'>
+            {minPrice ? <Text>￥{minPrice}起送</Text> : null} 
+          </View>
+        </View>
+      </View>
+    );
+  }
+} 
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,6 +1,26 @@
 import Taro from '@tarojs/taro';
 const foodKey = 'foodKey';
 
+export function getEvent() {
+  return myEvent;
+}
+
+export function getAllFoodInfo() {
+  let store = Taro.getStorageSync(foodKey);
+  let allPrice = 0;
+  let allNum = 0;
+  if (store) {
+    Object.keys(store).map((key) => {
+      debugger
+      if (store[key]) {
+        allPrice += store[key].price * store[key].num;
+        allNum += store[key].num;
+      }
+    });
+    return { allPrice, allNum };
+  }
+}
+
 export function getFoodCount(food) {
   let store = Taro.getStorageSync(foodKey);
   if (store && store[food.id]) {
@@ -14,7 +34,7 @@ export function setFoodCount(food, num, type, callback) {
 
   if (food) {
     let store = Taro.getStorageSync(foodKey);
-    if(!store ){store = {};}
+    if (!store) { store = {}; }
     if (type == "add") {
       if (store[food.id]) {
         store[food.id].num = num + 1;
